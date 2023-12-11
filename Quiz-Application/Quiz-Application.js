ac48cc4614293643ce9bbbd4e63470395fc76d64
Dom3760 <142520274+Dom3760@users.noreply.github.com>
2023-12-11 10:22:00 -0800

    Add files via upload

---
+++ Quiz-Application/Quiz-Application.js
@@ -0,0 +1,54 @@
+function 
+
+
+const question1 = {
+    question: "",
+    answer: "",
+}
+
+const question2 = {
+    question: "",
+    answer: "",
+}
+
+const question3 = {
+    question: "",
+    answer: "",
+}
+
+const question4 = {
+    question: "",
+    answer: "",
+}
+
+const question5 = {
+    question: "",
+    answer: "",
+}
+
+const question6 = {
+    question: "",
+    answer: "",
+}
+
+const question7 = {
+    question: "",
+    answer: "",
+}
+
+const question8 = {
+    question: "",
+    answer: "",
+}
+
+const question9 = {
+    question: "",
+    answer: "",
+}
+
+const question10 = {
+    question: "",
+    answer: "",
+}
+
+var questions = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10]
