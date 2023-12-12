8947ced2a1dbaeaaefdf4ec52adb528f60a97226
Dom3760 <142520274+Dom3760@users.noreply.github.com>
2023-12-12 10:28:38 -0800

    Add files via upload

--- Quiz-Application/Quiz-Application.js
+++ Quiz-Application/Quiz-Application.js
@@ -2,53 +2,53 @@ function
 
 
 const question1 = {
-    question: "",
-    answer: "",
+    question: "What is the best-selling video game of all time?",
+    answer: "Tetris",
 }
 
 const question2 = {
-    question: "",
-    answer: "",
+    question: 'In what year was the game "Fortnite" first released?',
+    answer: "2017",
 }
 
 const question3 = {
-    question: "",
-    answer: "",
+    question: 'Which company developed the game series "The Legend of Zelda"?',
+    answer: "Nintendo",
 }
 
 const question4 = {
-    question: "",
-    answer: "",
+    question: 'What is the objective of the game "Tetris"?',
+    answer: "fitting falling blocks together",
 }
 
 const question5 = {
-    question: "",
-    answer: "",
+    question: 'Who is the main character in the game "Super Mario Bros."?',
+    answer: "Mario",
 }
 
 const question6 = {
-    question: "",
-    answer: "",
+    question: 'What genre does the game "Counter-Strike" belong to?',
+    answer: "FPS",
 }
 
 const question7 = {
-    question: "",
-    answer: "",
+    question: 'Which popular game features a battle royale mode and is set in the "Call of Duty" universe?',
+    answer: "Call of Duty: Warzone",
 }
 
 const question8 = {
-    question: "",
-    answer: "",
+    question: "What gaming console is produced by Sony Interactive Entertainment?",
+    answer: "PlayStation",
 }
 
 const question9 = {
-    question: "",
-    answer: "",
+    question: 'In the game "Pokémon," what type is effective against water-type Pokémon?',
+    answer: "Electric",
 }
 
 const question10 = {
-    question: "",
-    answer: "",
+    question: "What does RPG stand for in the context of video games?",
+    answer: "Role-playing game",
 }
 
 var questions = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10]
